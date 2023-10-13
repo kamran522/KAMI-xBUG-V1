a69944e9785603db5ae55602cb40a091534e20d1
kamran522 <107455935+kamran522@users.noreply.github.com>
2023-10-13 12:09:15 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,67 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//contact details
+global.ownernomer = "923323963788"
+global.ownername = "Kamran Hasil"
+global.ytname = "K A M I"
+global.socialm = "K A M I"
+global.location = "BALOCHISTAN"
+
+global.ownernumber = '923323963788'  //creator number
+global.ownername = 'Kamran Hasil' //owner name
+global.botname = 'K A M I ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+
+//sticker details
+global.packname = 'Sticker By'
+global.author = 'K A M I\n\nContact: +923323963788'
+
+//console view/theme
+global.themeemoji = '🪀'
+global.wm = "KAMI-xBUG-V1 Inc."
+
+//theme link
+global.link = 'NULL'
+
+//custom prefix
+global.prefa = ['']
+
+//false=disable and true=enable
+global.autoRecording = false //auto recording
+global.autoTyping = false //auto typing
+global.autorecordtype = false //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = false //auto update bio
+global.anti92 = false //auto block +92 
+global.autoread_status = true //auto view status/story
+
+//menu type 
+//v1 is image menu, 
+//v2 is link + image menu,
+//v3 is video menu,
+//v4 is call end menu
+global.typemenu = 'v3'
+
+//reply messages
+global.mess = {
+    done: 'Done !',
+    prem: 'This feature can be used by premium user only',
+    admin: 'This feature can be used by admin only',
+    botAdmin: 'This feature can only be used when the bot is a group admin ',
+    owner: 'This feature can be used by owner only',
+    group: 'This feature is only for groups',
+    private: 'This feature is only for private chats',
+    wait: 'In process... ',    
+    error: 'Error!',
+}
+
+global.thumb = fs.readFileSync('./XeonMedia/thumb.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
